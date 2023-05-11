28bc383f420b7b96eda19ed0a38aa76228c36ef4
Maleila <maleila.henrymitchell@gmail.com>
2023-05-11 15:14:08 -0700

    basic home page

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<template>
+    <div class ="spacer"></div>
+<h1>
+    Technowiccan Herbal Companion
+</h1>
+<h2>
+    A searchable herb database of Cunningham's Encyclopedia of Magical Herbs
+</h2>
+
+<h2> Search by:</h2>
+
+<button> Name</button>
+<br>
+<button> Scientific Name</button>
+<br>
+<button> Property</button>
+<br>
+<button> Planet</button>
+</template>
+
+<style>
+h1 {
+    font-family: "Trattatello";
+    text-align: center;
+    font-size: 6em;
+    line-height: 0.75em;
+    margin: 0.5em;
+}
+
+h2 {
+    font-family: inherit;
+    font-weight: 100;
+    text-align: center;
+}
+
+.spacer {
+  height: 6em;
+}
+</style>
