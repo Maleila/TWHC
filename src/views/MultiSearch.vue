7db73f5b38531d21c434bd7d5f5c299685387b40
Maleila <maleila.henrymitchell@gmail.com>
2024-07-25 14:35:12 -0700

    dropdown for advanced search

--- src/views/MultiSearch.vue
+++ src/views/MultiSearch.vue
@@ -3,7 +3,6 @@ import { useFirestore } from "vuefire";
 import { collection, getDocs } from "firebase/firestore"; 
 import { query, where } from "firebase/firestore";
 
-//TODO: add select search for each input field
 export default {
     data() {
         return {
@@ -15,11 +14,67 @@ export default {
             element: "",
             deities: "",
             properties: "",
+            nameList: [],
+            scientific_name_list: [],
+            folk_names_list: [],
+            planetList: ["earth", "mars", "moon", "saturn", "venus", "uranus", "sun", "pluto", "jupiter"], //missing some?? idk
+            elementList: ["earth", "air", "fire", "water"],
+            deitiesList: [],
+            propertiesList: []
         }
     },
+    async mounted() {
+        //const db = useFirestore(); //do I need this actually....
+        this.loadList();
+    },
     methods: {
+        async selectName(item){
+            this.name = item;
+        },
+        async selectScientificName(item) {
+            this.scientific_name = item;
+        },
+        async selectFolkName(item) {
+            this.folk_name = item;
+        },
+        async selectPlanet(item) {
+            this.planet = item;
+        },
+        async selectElement(item) {
+            this.element = item;
+        },
+        async selectDeity(item) {
+            this.deities = item;
+        },
+        async selectProperty(item) {
+            this.properties = item;
+        },
+        async loadList() {
+            const db = useFirestore();
+
+            const querySnapshot = await getDocs(collection(db, "plants"));
+            querySnapshot.forEach((doc) => {
+                this.nameList.push(`${doc.data().name}`);
+                this.scientific_name_list.push(`${doc.data().scientific_name}`);
+
+                doc.data().folk_names.forEach((item) => {
+                    if(!this.folk_names_list.includes(item) && item.toLowerCase() != "na"){
+                        this.folk_names_list.push(item);
+                    }   
+                });
+                doc.data().deities.forEach((item) => {
+                    if(!this.deitiesList.includes(item) && item.toLowerCase() != "na"){
+                        this.deitiesList.push(item);
+                    }   
+                });
+                doc.data().properties.forEach((item) => {
+                    if(!this.propertiesList.includes(item) && item.toLowerCase() != "na"){
+                        this.propertiesList.push(item);
+                    }   
+                });
+            });
+        },
         async search() {
-            this.process();
             this.results = [];
             //idea from this stackoverflow post
             //https://stackoverflow.com/questions/69875064/firebase-version-9-using-multiple-conditional-where-clauses
@@ -33,7 +88,7 @@ export default {
                 q = query(q, where("scientific_name", "==", this.scientific_name));
             }
             if(this.folk_name) {
-                q = query(q, where("folk_name", "array-contains", this.folk_name.name));
+                q = query(q, where("folk_names", "array-contains", this.folk_name));
             }
             if(this.planet) {
                 q = query(q, where("planet", "==", this.planet));
@@ -56,15 +111,6 @@ export default {
             });
             this.reset();
         },
-        process() {
-            this.name = this.name.toLowerCase();
-            this.scientific_name = this.scientific_name.toLowerCase();
-            this.folk_name = this.folk_name.toLowerCase();
-            this.planet = this.planet.toLowerCase();
-            this.element = this.element.toLowerCase();
-            this.deities = this.deities.toLowerCase();
-            this.properties = this.properties.toLowerCase();
-        },
         reset() {
             this.name = "";
             this.scientific_name = "";
@@ -73,6 +119,14 @@ export default {
             this.element = "";
             this.deities = "";
             this.properties = "";
+
+            this.$refs.nameInput.clearInput();
+            this.$refs.scientificNameInput.clearInput();
+            this.$refs.folkNameInput.clearInput();
+            this.$refs.elementInput.clearInput();
+            this.$refs.planetInput.clearInput();
+            this.$refs.detiiesInput.clearInput();
+            this.$refs.propertiesInput.clearInput();
         },
         toInfo(param) {
             const plant = param;
@@ -86,21 +140,105 @@ export default {
 <h2>
     Advanced Search
 </h2>
-<br>
-Name: <input v-model="name" placeholder="name">
-<br>
-Scientific Name: <input v-model="scientific_name" placeholder="scientific name">
-<br>
-Folk Name: <input v-model="folkStr" placeholder="folk names">
-<br>
-Planet: <input v-model="planet" placeholder="planet">
-<br>
-Element: <input v-model="element" placeholder="element">
-<br>
-Deities: <input v-model="deitiesStr" placeholder="deities">
-<br>
-Properties: <input v-model="propertiesStr" placeholder="properties">
-<br>
+<br> <!--actually is there a way to use a v-model here instead of linking via refs and functions-->
+Name: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="nameInput"
+            placeholder="Name"
+            :items="nameList" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectName"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+</div>
+Scientific Name: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="scientificNameInput"
+            placeholder="Scientific Name"
+            :items="scientific_name_list" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectScientificName"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+    </div>
+Folk Name: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="folkNameInput"
+            placeholder="Folk name"
+            :items="folk_names_list" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectFolkName"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+    </div>
+Planet: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="planetInput"
+            placeholder="Planet"
+            :items="planetList" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectPlanet"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+    </div>
+Element: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="elementInput"
+            placeholder="Element"
+            :items="elementList" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectElement"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+    </div>
+Deities: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="detiiesInput"
+            placeholder="Deity"
+            :items="deitiesList" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectDeity"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+    </div>
+Properties: <div class="dropdown">
+    <vue3-simple-typeahead
+            ref="propertiesInput"
+            placeholder="Property"
+            :items="propertiesList" 
+            :minInputLength="1"
+            :itemProjection="itemProjectionFunction"
+            @selectItem="selectProperty"
+            @onInput="onInputEventHandler"
+            @onFocus="onFocusEventHandler"
+            @onBlur="onBlurEventHandler"
+        >
+        </vue3-simple-typeahead>
+    </div>
 <br>
 <button @click="search">
     Search
